229bb597ccee02a7252b2eba153da6789fbd42d5
Derajkumar <83283105+Derajkumar@users.noreply.github.com>
2026-09-21 22:52:58 +0530

    Fix Pages router path

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Home from './pages/Home'
 import { About, Achievements, Contact, Education, Experience, Projects, Services, Skills } from './pages/InnerPages'
 
 function App() {
-  return <BrowserRouter><Layout><Routes><Route path="/" element={<Home />} /><Route path="/about" element={<About />} /><Route path="/education" element={<Education />} /><Route path="/experience" element={<Experience />} /><Route path="/skills" element={<Skills />} /><Route path="/services" element={<Services />} /><Route path="/projects" element={<Projects />} /><Route path="/achievements" element={<Achievements />} /><Route path="/contact" element={<Contact />} /></Routes></Layout></BrowserRouter>
+  return <BrowserRouter basename="/portfolio"><Layout><Routes><Route path="/" element={<Home />} /><Route path="/about" element={<About />} /><Route path="/education" element={<Education />} /><Route path="/experience" element={<Experience />} /><Route path="/skills" element={<Skills />} /><Route path="/services" element={<Services />} /><Route path="/projects" element={<Projects />} /><Route path="/achievements" element={<Achievements />} /><Route path="/contact" element={<Contact />} /></Routes></Layout></BrowserRouter>
 }
 
 export default App
