f179702ae951f71231c8b297efbfe518c707b57c
Derajkumar <83283105+Derajkumar@users.noreply.github.com>
2026-09-21 22:31:53 +0530

    Organize source files

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Layout } from './components/Layout'
+import Home from './pages/Home'
+import { About, Achievements, Contact, Education, Experience, Projects, Services, Skills } from './pages/InnerPages'
+
+function App() {
+  return <BrowserRouter><Layout><Routes><Route path="/" element={<Home />} /><Route path="/about" element={<About />} /><Route path="/education" element={<Education />} /><Route path="/experience" element={<Experience />} /><Route path="/skills" element={<Skills />} /><Route path="/services" element={<Services />} /><Route path="/projects" element={<Projects />} /><Route path="/achievements" element={<Achievements />} /><Route path="/contact" element={<Contact />} /></Routes></Layout></BrowserRouter>
+}
+
+export default App
